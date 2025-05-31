0d2d2c12094c517db8213f0b1a2ed3c86f9acce6
Mattias <mattias.gijsels@hotmail.com>
2025-05-31 15:17:49 +0200

    added swagger to program.cs

--- PetQuestV1/PetQuestV1/Program.cs
+++ PetQuestV1/PetQuestV1/Program.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models; // Add this using directive
 using PetQuestV1.Client.Pages;
 using PetQuestV1.Components;
 using PetQuestV1.Components.Account;
 using PetQuestV1.Contracts.Defines;
 using PetQuestV1.Data;
-using PetQuestV1.Data.Defines; // For ISpeciesRepository, IPetRepository
-using PetQuestV1.Data.Repository; // For SpeciesRepository, PetRepository
+using PetQuestV1.Data.Defines;
+using PetQuestV1.Data.Repository;
 using PetQuestV1.Services;
 using PetQuestV1.Contracts.Models;
 
@@ -19,6 +20,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddControllers(); // Add this line to enable API controllers
+builder.Services.AddEndpointsApiExplorer(); // Add this line
+builder.Services.AddSwaggerGen(c => // Add this block for Swagger generation
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PetQuestV1 API", Version = "v1" });
+});
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
@@ -77,6 +85,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
     app.UseMigrationsEndPoint();
+    app.UseSwagger(); // Add this line
+    app.UseSwaggerUI(c => // Add this block for Swagger UI
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PetQuestV1 API V1");
+    });
 }
 else
 {
@@ -95,5 +108,7 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+
+app.MapControllers(); // Add this line to map API controllers
 
 app.Run();
